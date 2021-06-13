00c7f97b15974d33274f43cb610de4b9d9a40c5c
betul <bet.er@hotmail.com>
2021-06-13 15:51:31 +0300

    add remove note feature

--- src/components/NoteDetail.js
+++ src/components/NoteDetail.js
@@ -1,48 +1,79 @@
-import React, {useState, useEffect} from 'react';
-import 'antd/dist/antd.css';
-import {useParams} from "react-router-dom";
-import { SmileOutlined, PlusOutlined, DeleteOutlined } from '@ant-design/icons';
-import { Button, Input  } from 'antd';
-import db from '../firebaseConfig'
+import React, { useState, useEffect } from "react";
+import "antd/dist/antd.css";
+import { useParams } from "react-router-dom";
+import { SmileOutlined, PlusOutlined, DeleteOutlined } from "@ant-design/icons";
+import { Button, Input, List, Typography, Divider, Skeleton } from "antd";
+import db from "../firebaseConfig";
+import firebase from "firebase/app";
+import "firebase/firestore";
 
 function NoteDetail() {
-    const{id} = useParams();  
+  const { id } = useParams();
 
-    const [content, setContent] = useState('');
-    const [notes, setAllNotes] = useState([]);
-    
-    const addContent = e => {
-      if(content !== '') {
-        db.collection('pages').doc(id).update({
-          "content": notes ? [...notes, content] :  [content],
-      })
-      .then(() => {
-        console.log("Data successfully written!");
-        setContent("")
-    })
-      }
+  const [content, setContent] = useState("");
+  const [notes, setAllNotes] = useState([]);
+
+  const addContent = (e) => {
+    if (content !== "") {
+      db.collection("pages")
+        .doc(id)
+        .update({
+          // content: notes ? [...notes, content] : [content],
+          content: firebase.firestore.FieldValue.arrayUnion(content),
+        })
+        .then(() => {
+          console.log("Data successfully written!");
+        });
     }
+  };
 
-    const fetchData = async () =>{
-      const noteRes = await db.collection('pages').doc(id).get() 
+  const fetchData = async () => {
+    const noteRes = await db.collection("pages").doc(id).get();
     //  console.log(noteRes.data().content)
-      setAllNotes(noteRes.data().content);
-    }
+    noteRes.data().content && setAllNotes(noteRes.data().content);
+  };
 
-    useEffect(()=>{
-      fetchData()
-    },[content])
+  const deleteNote = (e) => {
+    let del = e.target.innerHTML;
+    console.log(del);
+    // db.collection("pages")
+    //   .doc(id)
+    //   .delete()
+    //   .then(() => {
+    //     console.log("Document successfully deleted!");
+    //   });
+    // var contentRef = db.collection("pages").doc(id);
+    setAllNotes(notes.filter(item => item !== del));
+    var removeNote = db.collection("pages").doc(id).update({
+      content: firebase.firestore.FieldValue.arrayRemove(del),
+    });
+    console.log(notes)
+    return removeNote;
+  };
 
-    return (
-      <div>
-           NoteDetail Page
-        <div>{id}</div>
-        My Notes: {console.log(notes)}
+  useEffect(() => {
+    fetchData();
+  }, [content]);
 
-        <Input placeholder="Add Note" onChange={e => setContent(e.target.value)}/>
-          <Button icon={<PlusOutlined />} onClick={addContent}/>
-      </div>
-    );
+  return (
+    <div>
+      NoteDetail Page
+      <Divider orientation="left">My Notes: </Divider>
+      <List
+        size="large"
+        bordered
+        dataSource={notes}
+        renderItem={(item) => (
+          <List.Item onClick={deleteNote}>{item} </List.Item>
+        )}
+      />
+      <Input
+        placeholder="Add Note"
+        onChange={(e) => setContent(e.target.value)}
+      />
+      <Button icon={<PlusOutlined />} onClick={addContent} />
+    </div>
+  );
 }
 
 export default NoteDetail;
